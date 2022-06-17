8ae91789bf576fcb91de15bb2d54876f8a8e5c0e
kapil tawari <kapiltawari1008@gmail.com>
2022-06-18 02:40:35 +0530

    Initial commit

---
+++ GradingSystem.java
@@ -0,0 +1,8 @@
+//grading system of a college
+public class GradingSystem {
+    public static void main(String[] args) {
+        System.out.println("Enter marks of the student=");
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
